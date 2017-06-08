1704fab44ccfe8cfd88c04fe6d6887d6f9f2818d
Gihwan Kim <ghkim3221@gmail.com>
2017-06-08 19:24:05 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const http = require('http');
+
+app.set('views', path.join(__dirname, 'app', 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'mindmapjs',
+    resave: false,
+    saveUninitialized: true
+}));
+
+var port = process.env.PORT | 3000;
+var server = http.createServer(app);
+
+server.on('error', onError);
+server.on('listening', onListening);
+server.listen(port);
+
+function onListening() {
+    console.log("Listening on " + server.address().port);
+}
+
+function onError(err) {
+    if (err.syscall !== 'listen') {
+        throw err;
+    }
+
+    switch (err.code) {
+        case 'EACCES':
+            console.error(port + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(port + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw err;
+    }
+}
