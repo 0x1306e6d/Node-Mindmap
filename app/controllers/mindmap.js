180838f42a40d56a8eee6f3c75ebeeada73c6df0
Gihwan Kim <ghkim3221@gmail.com>
2017-06-11 20:40:00 +0900

    Add mindmap page

--- app/controllers/mindmap.js
+++ app/controllers/mindmap.js
@@ -3,6 +3,23 @@ const router = express.Router();
 
 const Mindmap = require('../models/mindmap');
 
+router.get('/:id', function (req, res) {
+    var id = req.params.id;
+    Mindmap.findOne({
+        _id: id,
+        owener: req.user._id
+    }, function (err, mindmap) {
+        if (err) {
+            console.error("Failed to find mindmap. id: " + id);
+            return;
+        }
+
+        res.render('layouts/mindmap', {
+            mindmap: mindmap
+        });
+    });
+});
+
 router.post('/', function (req, res) {
     var title = req.body.title;
     Mindmap.create(req.user._id, title, function (err, mindmap) {
