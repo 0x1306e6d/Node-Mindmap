4012aa06cc6ebe045529da4a9e620aa00311ae58
Gihwan Kim <ghkim3221@gmail.com>
2017-06-11 21:11:56 +0900

    Visualize mindmap

--- resources/js/mindmap.js
+++ resources/js/mindmap.js
@@ -1,10 +1,60 @@
+var svg = d3.select('#mindmap');
+var g = svg.append('g').attr("transform", "translate(40,0)");
+var tree = d3.tree().size([800, 600]);
+
 function load(id) {
     var socket = io();
     socket.on('nodes', onNodes);
-
     socket.emit('nodes', id);
 }
 
 function onNodes(data) {
-    console.log("onNodes data: " + JSON.stringify(data));
+    var root = d3.hierarchy(data);
+    tree(root);
+    draw(root);
 }
+
+function draw(root) {
+    var link = g.selectAll(".link")
+        .data(root.descendants().slice(1))
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", function (d) {
+            return "M" + d.y + "," + d.x + "C" + (d.y + d.parent.y) / 2 + "," + d.x + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x + " " + d.parent.y + "," + d.parent.x;
+        });
+
+    var node = g.selectAll(".node")
+        .data(root.descendants())
+        .enter()
+        .append("g")
+        .attr("class", function (d) {
+            return "node" + (d.children ? " node--internal" : " node--leaf");
+        })
+        .attr("transform", function (d) {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+
+    node.append("circle")
+        .attr("r", 2.5);
+
+    node.append("text")
+        .attr("dy", 3)
+        .attr("x", function (d) {
+            return d.children ? -10 : 10;
+        })
+        .style("text-anchor", function (d) {
+            return d.children ? "end" : "start";
+        })
+        .text(function (d) {
+            return d.data.name
+        });
+}
+
+$(window).on('resize', function () {
+    var width = $('#mindmap-container').width();
+    var height = $(document).height() * 0.8;
+
+    svg.attr('width', width);
+    svg.attr('height', height);
+});
+$(window).resize();
